e6c5e7a58af6f854dae67a710c039bcdb5fcfd21
RoboTums <t.rack721@gmail.com>
2020-04-05 19:03:17 -0400

    setting up Cython Environment

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+from distutils.core import setup
+from Cython.Build import cythonize
+import numpy
+
+setup(
+	name="TSP ART",
+    version="0.0.1",
+    author="Tumas Rackaitis",
+    author_email="trackait@oberlin.edu",
+    description="Solving TSP with Art",
+	packages=setuptools.find_packages(),
+	ext_modules = cythonize(
+		[
+		'stipple.pyx'
+		]
+		,        
+	compiler_directives={'language_level': "3"}),
+	include_dirs=[numpy.get_include()],
+	install_requires=['numpy'] 
+	)
---
+++ stipple.pyx
@@ -0,0 +1,12 @@
+"""
+	Stipples image provided in by command line.
+	Creates an associated postscript file and .cyc file. 
+"""
+
+__author__ = 'Tumas Rackaitis'
+
+import numpy as np
+
+cdef int stipple( int num_dots, int num_zones=10, int num_outer_iters=10, int iter_multiple=1000, double exp=1.3):
+	print('works.')
+
